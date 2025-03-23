c4353045fb955afb70005e4a98d02e0c0af72e1c
Sebastian Armata <sebastian.armata22@gmail.com>
2025-03-23 09:55:05 +0100

    feat(users): init users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProjectsModule],
+  imports: [ProjectsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
